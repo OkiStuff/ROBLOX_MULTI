4a736bebe413c373080f1992ef8313aeaf0272a0
Frankie A <40648091+OkiStuff@users.noreply.github.com>
2022-12-14 19:28:37 -0500

    Update and rename ROBLOX_MULTI.cpp to ROBLOX_MULTI.c

--- ROBLOX_MULTI.c
+++ ROBLOX_MULTI.c
@@ -1,11 +1,12 @@
-#include "stdafx.h"
 #include <Windows.h>
+#include <tchar.h>
+#include <SDKDDKVer.h>
 #include <stdio.h>
 #include <conio.h>
 
 int main()
 {
-	CreateMutex(NULL, TRUE, L"ROBLOX_singletonMutex"); //bInitialOwner is TRUE, takes ownership and holds it until this process exits
+	CreateMutex(NULL, 1, L"ROBLOX_singletonMutex"); //bInitialOwner is TRUE, takes ownership and holds it until this process exits
 
 	printf("You can now use multiple ROBLOX clients. Closing this will cause all ROBLOX clients to terminate except for one.\n");
 	printf("Press any key to close");
